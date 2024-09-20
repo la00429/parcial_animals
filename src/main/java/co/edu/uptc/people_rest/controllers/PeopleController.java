c8d2c73eaf3f72a09333524b1ebfece3512ed427
German Amezquita <german.amezquita@gmail.com>
2024-09-20 13:47:40 -0500

    add logger

--- src/main/java/co/edu/uptc/people_rest/controllers/PeopleController.java
+++ src/main/java/co/edu/uptc/people_rest/controllers/PeopleController.java
@@ -3,6 +3,8 @@ package co.edu.uptc.people_rest.controllers;
 import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +19,16 @@ import co.edu.uptc.people_rest.services.PeopleService;
 @RestController
 @RequestMapping("/people")
 public class PeopleController {
+
+ private static final Logger logger = LoggerFactory.getLogger(PeopleController.class);
+
    @Autowired
     private PeopleService peopleService; 
 
 
     @GetMapping("/range")
     public List<String> getNombres(@RequestParam int from, @RequestParam int to) throws IOException {
+        logger.info("getNombres called with parameters: from = {}, to = {}", from, to);
         return peopleService.getPersonInRange(from, to);
     }
 
